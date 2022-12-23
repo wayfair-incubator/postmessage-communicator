d157aa68a09f655bfb5b69a7963cc5eb926a9229
Utsav Beri <uberi@wayfair.com>
2022-12-23 16:50:54 -0500

    init

--- src/classes/ClientCommunicator.ts
+++ src/classes/ClientCommunicator.ts
@@ -3,6 +3,7 @@ import {Communicator} from './Communicator';
 export enum ClientEvent {
   LoginWithToken = 'LoginWithToken',
   RefreshWithToken = 'RefreshWithToken',
+  AddToCartStatus = 'AddToCartStatus',
 }
 
 interface InitPayload {
@@ -10,11 +11,20 @@ interface InitPayload {
   landingExperience: string;
 }
 
+interface AddToCartStatusPayload {
+  success: boolean;
+  message: string;
+  projectId: string;
+  versionId: string;
+}
 export class ClientCommunicator extends Communicator {
   init(payload: InitPayload): void {
     this.post({type: ClientEvent.LoginWithToken, payload});
   }
   refreshWithToken(token: string): void {
     this.post({type: ClientEvent.RefreshWithToken, payload: token});
+  }
+  addToCartStatus(payload: AddToCartStatusPayload): void {
+    this.post({type: ClientEvent.AddToCartStatus, payload: payload})
   }
 }
--- src/classes/VendorCommunicator.ts
+++ src/classes/VendorCommunicator.ts
@@ -11,6 +11,7 @@ export enum VendorEvent {
   TrackingEvent = 'TrackingEvent',
   TokenRefreshRequested = 'TokenRefreshRequested',
   UnauthorizedToken = 'UnauthorizedToken',
+  continueToCart = 'ContinueToCart',
 }
 
 interface Metadata {
@@ -84,5 +85,8 @@ export class VendorCommunicator extends Communicator {
 
   unauthorizedToken(error: string): void {
     this.post({type: VendorEvent.UnauthorizedToken, payload: error});
+  }
+  continueToCart(payload: EventPayload): void {
+    this.post({type: VendorEvent.ContinueToCart, payload})
   }
 }
